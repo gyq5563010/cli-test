96975df0e016bc25104d56fe839191d2ec7de539
gyq5563010 <gyq5563010@users.noreply.github.com>
2019-10-18 03:35:13 +0000

    Deploying to gh-pages from master 596a8e7e440bbe08c309df90403e4487cc3ba72e

---
+++ precache-manifest.e29c63e338cfe8c3b48ffadc3aa37a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffef44c3d817d19f4bb1c78524ff3b54",
+    "url": "/cli-test/index.html"
+  },
+  {
+    "revision": "4e1ef9770f44c2739d84",
+    "url": "/cli-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "11c5434cd883b1d03a88",
+    "url": "/cli-test/static/js/2.93f74234.chunk.js"
+  },
+  {
+    "revision": "4e1ef9770f44c2739d84",
+    "url": "/cli-test/static/js/main.dd880453.chunk.js"
+  },
+  {
+    "revision": "c5b11a7ff55d0af8c2cd",
+    "url": "/cli-test/static/js/runtime-main.26fb1781.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cli-test/static/media/logo.25bf045c.svg"
+  }
+]);
